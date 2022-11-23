9552c52570ae39fd4e732a1021f314ec55c0eacb
eduarm123 <eduardoaoalex@gmail.com>
2022-11-23 18:28:29 +0000

    model has been modified

--- src/models.py
+++ src/models.py
@@ -11,12 +11,27 @@ Base = declarative_base()
 
 
 # Our models. Remove the demo "Note" and add your own
+  
 
-class Recipe(Base):
+class Recipe(Base): # Wednesday
     __tablename__="recipe"
     id = Column(Integer, primary_key=True)
     title = Column(String(30))
+    contains = relationship("Contains",back_populates="recipe")
+
+class Ingredients(Base): # Thursday
+    __tablename__="ingredients"
+    id = Column(Integer, primary_key=True)
     content = Column(Text)
-
-
-
+    contains = relationship("Contains",back_populates="ingredients")
+     
+class Contains(Base):
+    __tablename__="contains"
+    id = Column(Integer, primary_key=True)
+    
+    recipe= relationship("Recipe", back_populates="containRecepies")
+    ingredients = relationship("Ingredients", back_populates="containIngredients")
+      
+    containIngredients=Column(Integer, ForeignKey("contains.ingredients"))
+    containRecepies=Column(Integer, ForeignKey("contains.recipe"))
+  
