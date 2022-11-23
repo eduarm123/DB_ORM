7b1063668d18bd8173bd250ce19beb239ca3df1e
eduarm123 <eduardoaoalex@gmail.com>
2022-11-23 18:38:36 +0000

    I modified recipe

--- src/recipe.py
+++ src/recipe.py
@@ -17,7 +17,9 @@ def menu():
     print("2: List recipes")
     print("3: Add new recipies")
     print("4: View recipies")
-    print("5: Delete recipe")
+    print("5: Add ingredients")
+    print("6: View ingredients")
+    print("7: Delete recipe")
     print("0: Quit")
     print()
     return int(input("Choose option: "))
@@ -39,14 +41,14 @@ if __name__=="__main__":
             for recipe in recipes:
                 print(f"{recipe.id}: {recipe.title}")
                 
-        elif userInput==3:
+        elif userInput==3: # Add new recipies
             title=input("Enter new recipe title: ")
-            content=input("Enter new recipe instructions: ")
-            newRecipe = models.Recipe(title=title,content=content)
+            #content=input("Enter new recipe instructions: ")
+            newRecipe = models.Recipe(title=title)
             session.add(newRecipe)    
             session.commit()
 
-        elif userInput==4:
+        elif userInput==4: # View recipies
             recipeNum=int(input("Enter recipe number: "))
             recipe = session.query(models.Recipe).filter_by(id=recipeNum).first()
             if recipe==None:
@@ -55,8 +57,30 @@ if __name__=="__main__":
                 print(recipe.title)
                 print("-"*len(recipe.title))
                 print(recipe.content)
+                
+        elif userInput==5: # Add ingredients
+            ingredientNum=int(input("Enter recipe number: "))
+            recipe = session.query(models.Contains).filter_by(id=ingredientNum).first()
+            if recipe==None:
+                print("There is no recipe with that number")
+            else:
+                ingredientsCon=input("Enter ingredients ")
+                newIngredient = models.Contains(ingredientsCon=ingredientsCon, containRecepies=recipe)
+                session.add(newIngredient)    
+                session.commit()
+            
+        elif userInput==6: # View ingredients
+            ingredientNum=int(input("Enter recipe number: "))
+            recipe = session.query(models.Recipe).filter_by(id=ingredientNum).first()
+            if recipe==None:
+                print("There is no recipe with that number")
+            else:
+                print(recipe.title)
+                print("-"*len(recipe.title))
+                print(recipe.content)
+                
 
-        elif userInput==5:
+        elif userInput==7:
             recipeNum=int(input("Enter recipe number: "))
             recipe = session.query(models.Recipe).filter_by(id=recipeNum)
             if recipe==None:
